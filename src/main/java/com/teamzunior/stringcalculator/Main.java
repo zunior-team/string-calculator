0f65f5e30a797772fe2c23176d1709dc06a40559
nokchax <nokchax@gmail.com>
2020-01-08 11:14:14 +0900

    project init

---
+++ src/main/java/com/teamzunior/stringcalculator/Main.java
@@ -0,0 +1,7 @@
+package com.teamzunior.stringcalculator;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/teamzunior/stringcalculator/StringCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamzunior.stringcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
