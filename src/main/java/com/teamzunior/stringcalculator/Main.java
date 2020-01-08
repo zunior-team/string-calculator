ad9e6382ed8b45706c6621940476bc4004207f56
nokchax <nokchax@gmail.com>
2020-01-09 08:23:31 +0900

    feat : implements main method

--- src/main/java/com/teamzunior/stringcalculator/Main.java
+++ src/main/java/com/teamzunior/stringcalculator/Main.java
@@ -1,7 +1,21 @@
 package com.teamzunior.stringcalculator;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.print("수식을 입력해주세요(종료를 원하실경우 exit) : ");
+            String input = scanner.nextLine();
+
+            if("exit".equalsIgnoreCase(input)) {
+                return;
+            }
+
+            System.out.println("계산 결과는 : " + Interpreter.interpret(input));
+        }
 
     }
 }
