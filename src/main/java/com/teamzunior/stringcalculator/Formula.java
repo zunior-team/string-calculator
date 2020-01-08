572691effba1e4a417ddf290fe0533a913d8f2e4
nokchax <nokchax@gmail.com>
2020-01-08 20:36:02 +0900

    feat : apply operator enum

--- src/main/java/com/teamzunior/stringcalculator/Formula.java
+++ src/main/java/com/teamzunior/stringcalculator/Formula.java
@@ -3,27 +3,22 @@ package com.teamzunior.stringcalculator;
 import java.util.Objects;
 
 public class Formula {
-    private String formulaString;
     private int operand;
-    private String operator;
+    private Operator operator;
     private Formula formula;
 
     public Formula() {
     }
 
-    public Formula(int operand, String operator, String formulaString) {
+    public Formula(int operand, Operator operator, String formulaString) {
         this.operand = operand;
         this.operator = operator;
         this.formula = Interpreter.interpret(formulaString);
     }
 
-    public Formula(String formulaString) {
-        this.formulaString = formulaString;
-    }
-
-    public Formula(int operand, String formulaString) {
+    public Formula(int operand, Operator operator) {
         this.operand = operand;
-        this.formulaString = formulaString;
+        this.operator = operator;
     }
 
     @Override
@@ -32,13 +27,12 @@ public class Formula {
         if (o == null || getClass() != o.getClass()) return false;
         Formula formula1 = (Formula) o;
         return operand == formula1.operand &&
-                Objects.equals(formulaString, formula1.formulaString) &&
                 Objects.equals(operator, formula1.operator) &&
                 Objects.equals(formula, formula1.formula);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(formulaString, operand, operator, formula);
+        return Objects.hash(operand, operator, formula);
     }
 }
--- src/main/java/com/teamzunior/stringcalculator/Interpreter.java
+++ src/main/java/com/teamzunior/stringcalculator/Interpreter.java
@@ -2,15 +2,28 @@ package com.teamzunior.stringcalculator;
 
 import org.springframework.util.StringUtils;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static com.teamzunior.stringcalculator.Operator.*;
+
 public class Interpreter {
     private static final Pattern pattern = Pattern.compile("(\\ ?-?\\d+)(\\ ?[+|\\-|*|/]\\ ?)(.*)");
+    private static final Map<String, Operator> operators;
 
     private static final int OPERAND_INDEX = 1;
     private static final int OPERATOR_INDEX = 2;
     private static final int FORMULA_INDEX = 3;
+
+    static {
+        operators = new HashMap<>();
+        operators.put("+", PLUS);
+        operators.put("-", MINUS);
+        operators.put("*", MULTIPLY);
+        operators.put("/", DIVIDE);
+    }
 
     public static Formula interpret(String formula) {
         if(isInvalidInput(formula)) {
@@ -20,14 +33,14 @@ public class Interpreter {
         formula = formula.trim();
         Matcher matcher = pattern.matcher(formula);
         if(!matcher.find()) {
-            return new Formula(interpretNumber(formula), formula);
+            return new Formula(interpretNumber(formula), NUM);
         }
 
         int operand = interpretNumber(matcher.group(OPERAND_INDEX).trim());
         String operator = matcher.group(OPERATOR_INDEX).trim();
         String subFormula = matcher.group(FORMULA_INDEX).trim();
 
-        return new Formula(operand, operator, subFormula);
+        return new Formula(operand, operators.get(operator), subFormula);
     }
 
     private static int interpretNumber(String formula) {
