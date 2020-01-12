f5b6445f9e6a2f134623c1204932c3b67b44d6d7
최준우 <junwoochoi@zuminternet.com>
2020-01-12 17:45:10 +0900

    refactor : validate 책임 위임 및 StringCalculator내부에서 직접 계산

--- src/main/java/com/teamzunior/stringcalculator/calculator/InputListener.java
+++ src/main/java/com/teamzunior/stringcalculator/calculator/InputListener.java
@@ -19,8 +19,6 @@ public class InputListener {
         while (!END_SIGN.equalsIgnoreCase(input)) {
             final String[] tokens = input.split(BLANK_DELIMITER);
 
-            validateElementsCount(tokens);
-
             final int result = StringCalculator.calculate(tokens);
 
             System.out.println(result);
@@ -31,19 +29,14 @@ public class InputListener {
         printEndMessage();
     }
 
-    private void printEndMessage() {
-        System.out.println("#################        EXIT       #####################");
-    }
 
     private void printStartMessage() {
         System.out.println("################# LISTENING ...     #####################");
         System.out.println("################# PRESS q! to EXIT  #####################");
     }
 
-    private void validateElementsCount(String[] tokens) {
-        if (tokens.length < 3) {
-            throw new IllegalArgumentException("elements length must lager than 2");
-        }
+    private void printEndMessage() {
+        System.out.println("#################        EXIT       #####################");
     }
 
     private String getTrimmedNextLine() {
--- src/main/java/com/teamzunior/stringcalculator/calculator/StringCalculator.java
+++ src/main/java/com/teamzunior/stringcalculator/calculator/StringCalculator.java
@@ -5,8 +5,31 @@ public class StringCalculator {
     public static final String SUB_SIGN = "-";
     public static final String DIV_SIGN = "/";
     public static final String MULTI_SIGN = "*";
+    public static final int MINIMUM_EXPRESSION_LENGTH = 3;
+    public static final int EXPRESSION_INTERVAL = 2;
+
 
     public static int calculate(String[] elements) {
-        return new StringCalculatorElements(elements).calculateAll();
+        validateElements(elements);
+
+        int result = initFirstOperand(elements);
+        for (int index = 1; index < elements.length; index += EXPRESSION_INTERVAL) {
+            final CalculateType calculateType = Interpreter.interpretCalculateType(elements[index]);
+            final int calculateNumber = Interpreter.interpretNumber(elements[index + 1]);
+
+            result = Expression.newInstance(result, calculateNumber, calculateType)
+                    .result();
+        }
+        return result;
+    }
+
+    private static int initFirstOperand(String[] elements) {
+        return Interpreter.interpretNumber(elements[0]);
+    }
+
+    private static void validateElements(String[] elements) {
+        if (elements.length < MINIMUM_EXPRESSION_LENGTH) {
+            throw new IllegalArgumentException("elements length must lager than 2");
+        }
     }
 }
